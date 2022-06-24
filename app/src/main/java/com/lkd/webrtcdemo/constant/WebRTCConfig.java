b94753bf40623e6355d13f908edfca0db99f6db8
lkkings <90047484+lkkings@users.noreply.github.com>
2022-06-24 11:50:26 +0800

    Initial commit

---
+++ app/src/main/java/com/lkd/webrtcdemo/constant/WebRTCConfig.java
@@ -0,0 +1,32 @@
+package com.lkd.webrtcdemo.constant;
+
+public class WebRTCConfig {
+    /**
+     * 信令服务器地址
+     */
+    public static final String SOCKET_HOST = "https://s.lkd.pub:8082";
+    /**
+     * TURN服务器地址
+     */
+    public static final String TURN_HOST = "turn:119.8.38.146:3478";
+    /**
+     * TURN服务器用户名
+     */
+    public static final String TURN_USER = "haoye";
+    /**
+     * TURN服务器密码
+     */
+    public static final String TURN_PASS = "2893891716Aa";
+    /**
+     * STUN服务器
+     */
+    public static final String STUN_HOST = "stun:119.8.38.146:3478";
+    /**
+     *  音频ID
+     */
+    public static final String AUDIO_TRACK_ID = "AUDIO";
+    /**
+     * 视频ID
+     */
+    public static final String VIDEO_TRACK_ID = "VIDEO";
+}
